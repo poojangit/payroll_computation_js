3dd39377f01cac48f77343056c1c546351c8d174
poojangit <poojang147@gmail.com>
2025-06-19 11:37:03 +0530

    [POOJA] Attendance-Check : Modified a payroll problem by adding a employee object details and displaying their details along with checking the employee's presence

--- employee.js
+++ employee.js
@@ -1,7 +1,36 @@
+// Employee Payroll Application
 class EmployeePayroll {
-   displayMessage(){
-        console.log("Welcome to Employee Payroll Application");
-   }
+  // Constructor to initialize employee details
+  constructor(empId, empName) {
+    this.empId = empId;
+    this.empName = empName;
+    this.attendance = "";
+  }
+  displayMessage() {
+    console.log("Welcome to Employee Payroll Application");
+  }
+  // Method to mark attendance randomly
+  markAttendance() {
+    let isPresent = Math.floor(Math.random() * 2);
+    this.attendance = isPresent ? "Present" : "Absent";
+  }
+  // method to display employee details
+  displayDetails() {
+    console.log(
+      `Employee ID: ${this.empId}, Name: ${this.empName}, Attendance: ${this.attendance}`
+    );
+  }
 }
 const employeePayroll = new EmployeePayroll();
 employeePayroll.displayMessage();
+//create employee objects
+let empDetails = [
+  new EmployeePayroll(11, "Pooja"),
+  new EmployeePayroll(41, "Deepika"),
+  new EmployeePayroll(12, "Lakshmi"),
+];
+// Mark attendance for each employee and display their details
+empDetails.forEach((employee) => {
+  employee.markAttendance();
+  employee.displayDetails();
+});
