87f44c31e7fb07a61071346e67cda3b4c4cbdeea
poojangit <poojang147@gmail.com>
2025-06-19 10:25:37 +0530

    [POOJA] CREATED : created a javascript file for calculating the employee's wage and displayed a welcome message

---
+++ employee.js
@@ -0,0 +1,7 @@
+class EmployeePayroll {
+   displayMessage(){
+        console.log("Welcome to Employee Payroll Application");
+   }
+}
+const employeePayroll = new EmployeePayroll();
+employeePayroll.displayMessage();
