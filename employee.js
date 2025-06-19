03a1ac47fdbd721a53ae46d4c6172017838b9107
poojangit <poojang147@gmail.com>
2025-06-19 12:18:49 +0530

    [POOJA] ADDED Daily-Wage: Created a method to calculate a daily employee wage based on the employes presence

--- employee.js
+++ employee.js
@@ -5,7 +5,9 @@ class EmployeePayroll {
     this.empId = empId;
     this.empName = empName;
     this.attendance = "";
+    this.dailyWage = 0;
   }
+ 
   displayMessage() {
     console.log("Welcome to Employee Payroll Application");
   }
@@ -14,10 +16,21 @@ class EmployeePayroll {
     let isPresent = Math.floor(Math.random() * 2);
     this.attendance = isPresent ? "Present" : "Absent";
   }
+  //UC2 - For calculating daily wage based on attendance
+  calculateWage() {
+    const WAGE_PER_HOUR = 20;
+    const WORKING_HOURS = 8;
+    const dailyWage = WAGE_PER_HOUR * WORKING_HOURS;
+    if(this.attendance === "Present") {
+      this.dailyWage = dailyWage;
+    } else {
+      this.dailyWage = 0;
+    }
+  }
   // method to display employee details
   displayDetails() {
     console.log(
-      `Employee ID: ${this.empId}, Name: ${this.empName}, Attendance: ${this.attendance}`
+      `Employee ID: ${this.empId}, Name: ${this.empName}, Attendance: ${this.attendance}, Daily Wage: ${this.dailyWage}`
     );
   }
 }
@@ -32,5 +45,7 @@ let empDetails = [
 // Mark attendance for each employee and display their details
 empDetails.forEach((employee) => {
   employee.markAttendance();
+  employee.calculateWage(); //UC2 Implementing calculateWage method
   employee.displayDetails();
 });
+ 
